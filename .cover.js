23c1ec1fa3812059e8bcca4de3be3fb5bb1d3124
yaowj1218 <ywj159512186@gmail.com>
2024-05-31 10:53:40 +0800

    Update .cover.js

--- .cover.js
+++ .cover.js
@@ -1,9 +1,9 @@
 const path = require('path')
 
 module.exports = {
-  entry: './index.js',  // 项目入口
-  oldBranch: 'test',    // 需要对比的老分支
-  newBranch: 'master',  // 需要对比的新分支
+  entry: '/src/index.js',  // 项目入口
+  oldBranch: 'master',    // 需要对比的老分支
+  newBranch: 'test',  // 需要对比的新分支
   alias:{               // 配置webpack中的路径别名
     '@a': path.join(__dirname, 'src', 'moduleA'),
     '@b': path.join(__dirname, 'src', 'moduleB'),
